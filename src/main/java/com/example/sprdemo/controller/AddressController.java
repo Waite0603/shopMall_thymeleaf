33d6bd2f1012c30ee0fd1b07b5bfd67bd961ccc0
Waite Wang <waite@waite.wang>
2024-03-23 21:15:49 +0800

    feat: interface api of centerAddress and centerInfo

--- src/main/java/com/example/sprdemo/controller/AddressController.java
+++ src/main/java/com/example/sprdemo/controller/AddressController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/address")
+@RequestMapping("/api/address")
 public class AddressController {
     @Resource
     private AddressService addressService;
